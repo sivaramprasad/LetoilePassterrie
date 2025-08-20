f63ae9db87c77dc665dc7ffb7f4652e9a1c9e31f
Sivak <sivak@avetti.ca>
2025-08-20 16:13:31 +0530

    build issues resolved

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // tells Next.js to generate static HTML
+  basePath: '', // if your app is not in subfolder, leave blank
+  assetPrefix: '', // set CDN/static prefix if needed
+  images: {
+    unoptimized: true, // required for static export
+  },
+  trailingSlash: true, // helps with static hosting (adds / to URLs)
+};
 
 module.exports = nextConfig;
